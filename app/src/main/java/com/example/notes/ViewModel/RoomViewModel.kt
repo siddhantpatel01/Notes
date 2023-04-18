e05b930aa7c5802dbfb1d8ef98911ddee4278a3d
Siddhant Patel <Siddhantpatel445@gmail.com>
2023-04-18 18:41:52 +0530

    View model created

--- app/src/main/java/com/example/notes/ViewModel/RoomViewModel.kt
+++ app/src/main/java/com/example/notes/ViewModel/RoomViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.notes.ViewModel
 
-class RoomViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.notes.ROOM_DB.NoteDao
+import com.example.notes.ROOM_DB.Notes
+import com.example.notes.ROOM_DB.myDatabase
+import com.example.notes.Rapo.NoteRepo
+
+class RoomViewModel(application: Application) :AndroidViewModel(application) {
+    val allNotes : LiveData<List<Notes>>
+    init {
+        val dao = myDatabase.getDatabase(application).getNoteDao()
+        val repository = NoteRepo(dao)
+        allNotes = repository.allNotes
+    }
+
 }
