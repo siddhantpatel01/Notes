a7f31361c8d4bbc8cca09a0b884cfb1129bac8e1
Siddhant Patel <siddhantpatel445@gmail.com>
2023-05-12 11:45:35 +0530

    migration implimented

--- app/src/main/java/com/example/notes/ROOM_DB/myDatabase.kt
+++ app/src/main/java/com/example/notes/ROOM_DB/myDatabase.kt
@@ -4,14 +4,24 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 @Database(entities = [Notes::class], version = 2, exportSchema = false)
 abstract class myDatabase : RoomDatabase() {
     abstract fun getNoteDao() :NoteDao
 
     companion object {
+
         // Singleton prevents multiple instances of database opening at the
         // same time.
+
+        val migration_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE Notes ADD COLUMN Title TEXT NOT NULL DEFAULT ''")
+            }
+        }
+
         @Volatile
         private var INSTANCE: myDatabase? = null
 
@@ -22,8 +32,8 @@ abstract class myDatabase : RoomDatabase() {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     myDatabase::class.java,
-                    "word_database"
-                ).build()
+                    "Siddhant"
+                ).addMigrations(migration_1_2).build()
                 INSTANCE = instance
                 // return instance
                 instance
